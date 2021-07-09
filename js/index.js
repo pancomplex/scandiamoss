2e13008cbb5191feb6a4a0ad0b24218c544b1a3f
pancomplex <renaissance21c@gmail.com>
2021-07-09 18:23:11 +0900

    [7/9 day] burger menu 작업중

--- js/index.js
+++ js/index.js
@@ -3,100 +3,26 @@ $(document).on("scroll", function () {
   let scroll_top = $(this).scrollTop();
   let win_height = $(window).height();
   let page = Math.round(scroll_top / win_height) + 1;
-  console.log(page);
 
-  switch (page) {
-    case 1:
-      $(".mainBg")
-        .removeClass()
-        .addClass("mainBg page_" + page);
-      $(".topLogo")
-        .removeClass()
-        .addClass("topLogo page_" + page);
-      $(".topMenu")
-        .removeClass()
-        .addClass("topMenu page_" + page);
-      $("section.page_" + page)
-        .children()
-        .show();
-      $("section.page_" + page)
-        .siblings()
-        .children()
-        .hide();
-      break;
-    case 2:
-      $(".mainBg")
-        .removeClass()
-        .addClass("mainBg page_" + page);
-      $(".topLogo")
-        .removeClass()
-        .addClass("topLogo page_" + page);
-      $(".topMenu")
-        .removeClass()
-        .addClass("topMenu page_" + page);
-      $("section.page_" + page)
-        .children()
-        .show();
-      $("section.page_" + page)
-        .siblings()
-        .children()
-        .hide();
-      break;
-    case 3:
-      $(".mainBg")
-        .removeClass()
-        .addClass("mainBg page_" + page);
-      $(".topLogo")
-        .removeClass()
-        .addClass("topLogo page_" + page);
-      $(".topMenu")
-        .removeClass()
-        .addClass("topMenu page_" + page);
-      $("section.page_" + page)
-        .children()
-        .show();
-      $("section.page_" + page)
-        .siblings()
-        .children()
-        .hide();
-      break;
-    case 4:
-      $(".mainBg")
-        .removeClass()
-        .addClass("mainBg page_" + page);
-      $(".topLogo")
-        .removeClass()
-        .addClass("topLogo page_" + page);
-      $(".topMenu")
-        .removeClass()
-        .addClass("topMenu page_" + page);
-      $("section.page_" + page)
-        .children()
-        .show();
-      $("section.page_" + page)
-        .siblings()
-        .children()
-        .hide();
-      break;
-    case 5:
-      $(".mainBg")
-        .removeClass()
-        .addClass("mainBg page_" + page);
-      $(".topLogo")
-        .removeClass()
-        .addClass("topLogo page_" + page);
-      $(".topMenu")
-        .removeClass()
-        .addClass("topMenu page_" + page);
-      $("section.page_" + page)
-        .children()
-        .show();
-      $("section.page_" + page)
-        .siblings()
-        .children()
-        .hide();
-      break;
-    default:
-    //alert("스크롤 에러!!");
-  }
+  $(".mainBg")
+    .removeClass()
+    .addClass("mainBg page_" + page);
+  $(".topLogo")
+    .removeClass()
+    .addClass("topLogo page_" + page);
+  $(".topMenu")
+    .removeClass()
+    .addClass("topMenu page_" + page);
+  $("section.page_" + page)
+    .children()
+    .show();
+  $("section.page_" + page)
+    .siblings()
+    .children()
+    .hide();
+});
+
+// burger menu active
+$(".main_burgerBtn").on("click", function () {
+  $(".main_burgerBtn, .burger_container").toggleClass("act");
 });
