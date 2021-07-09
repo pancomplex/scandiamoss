1fdaad61ff57afbad6685e5d88d7c244db8edfbe
pancomplex <renaissance21c@gmail.com>
2021-07-09 11:27:25 +0900

    [7/9 day] html, css, js 작업중

--- js/index.js
+++ js/index.js
@@ -1,24 +1,100 @@
+// page change
 $(document).on("scroll", function () {
   let scroll_top = $(this).scrollTop();
   let win_height = $(window).height();
-  let page = Math.round(scroll_top / win_height);
+  let page = Math.round(scroll_top / win_height) + 1;
   console.log(page);
 
   switch (page) {
-    case 0:
-      $(".mainBg").removeClass().addClass("mainBg page_1");
-      break;
     case 1:
-      $(".mainBg").removeClass().addClass("mainBg page_even");
+      $(".mainBg")
+        .removeClass()
+        .addClass("mainBg page_" + page);
+      $(".topLogo")
+        .removeClass()
+        .addClass("topLogo page_" + page);
+      $(".topMenu")
+        .removeClass()
+        .addClass("topMenu page_" + page);
+      $("section.page_" + page)
+        .children()
+        .show();
+      $("section.page_" + page)
+        .siblings()
+        .children()
+        .hide();
       break;
     case 2:
-      $(".mainBg").removeClass().addClass("mainBg page_3");
+      $(".mainBg")
+        .removeClass()
+        .addClass("mainBg page_" + page);
+      $(".topLogo")
+        .removeClass()
+        .addClass("topLogo page_" + page);
+      $(".topMenu")
+        .removeClass()
+        .addClass("topMenu page_" + page);
+      $("section.page_" + page)
+        .children()
+        .show();
+      $("section.page_" + page)
+        .siblings()
+        .children()
+        .hide();
       break;
     case 3:
-      $(".mainBg").removeClass().addClass("mainBg page_even");
+      $(".mainBg")
+        .removeClass()
+        .addClass("mainBg page_" + page);
+      $(".topLogo")
+        .removeClass()
+        .addClass("topLogo page_" + page);
+      $(".topMenu")
+        .removeClass()
+        .addClass("topMenu page_" + page);
+      $("section.page_" + page)
+        .children()
+        .show();
+      $("section.page_" + page)
+        .siblings()
+        .children()
+        .hide();
       break;
     case 4:
-      $(".mainBg").removeClass().addClass("mainBg page_5");
+      $(".mainBg")
+        .removeClass()
+        .addClass("mainBg page_" + page);
+      $(".topLogo")
+        .removeClass()
+        .addClass("topLogo page_" + page);
+      $(".topMenu")
+        .removeClass()
+        .addClass("topMenu page_" + page);
+      $("section.page_" + page)
+        .children()
+        .show();
+      $("section.page_" + page)
+        .siblings()
+        .children()
+        .hide();
+      break;
+    case 5:
+      $(".mainBg")
+        .removeClass()
+        .addClass("mainBg page_" + page);
+      $(".topLogo")
+        .removeClass()
+        .addClass("topLogo page_" + page);
+      $(".topMenu")
+        .removeClass()
+        .addClass("topMenu page_" + page);
+      $("section.page_" + page)
+        .children()
+        .show();
+      $("section.page_" + page)
+        .siblings()
+        .children()
+        .hide();
       break;
     default:
     //alert("스크롤 에러!!");
