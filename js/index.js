03bc779ecde398bf1892fb7537424b5c5c8134d8
pancomplex <renaissance21c@gmail.com>
2021-07-10 03:54:53 +0900

    [7/9 night] 세부 애니메이션 작업중 (".topMenu")

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 // page change
 $(document).on("scroll", function () {
-  let scroll_top = $(this).scrollTop();
+  page_change();
+});
+
+let page_change = function () {
+  let scroll_top = $(document).scrollTop();
   let win_height = $(window).height();
   let page = Math.round(scroll_top / win_height) + 1;
 
@@ -20,9 +24,48 @@ $(document).on("scroll", function () {
     .siblings()
     .children()
     .hide();
-});
+
+  if (page > 1) $(".gotopBtn").addClass("on");
+  else $(".gotopBtn").removeClass("on");
+};
+
+// menu list translate
+let dic = {
+  "Online Shop": "온라인샵",
+  "About Us": "스칸디아모스",
+  Product: "스칸디아모스 제품",
+  "Educate Center": "교육센터",
+  "Art Center": "아트센터",
+  Community: "커뮤니티",
+  Gallery: "갤러리",
+  "Q&A": "질문과응답",
+  Video: "비디오",
+  News: "뉴스",
+  Notice: "게시판",
+  Contact: "문의하기",
+};
+
+let translate = function (input_txt) {
+  for (word in dic) {
+    if (input_txt == word) {
+      return dic[word];
+    } else if (input_txt == dic[word]) {
+      return word;
+    }
+  }
+};
+
+$(".topMenu li, .burgerMenu li")
+  .mouseenter(function () {
+    let en_txt = $(this).find("span").first().text();
+    $(this).find("span").first().text(translate(en_txt)).addClass("ko_txt");
+  })
+  .mouseleave(function () {
+    let ko_txt = $(this).find("span").first().text();
+    $(this).find("span").first().text(translate(ko_txt)).removeClass("ko_txt");
+  });
 
 // burger menu active
 $(".main_burgerBtn").on("click", function () {
-  $(".main_burgerBtn, .burger_container").toggleClass("act");
+  $(".main_burgerBtn, .burger_container, .topMenu").toggleClass("act");
 });
